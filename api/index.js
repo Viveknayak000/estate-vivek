9675606fce8370d2aa8e0d3c72ce644ff0b2432c
BHUKYA VIVEK <117837750+Viveknayak000@users.noreply.github.com>
2023-12-18 17:20:49 +0530

    mongodb database connect

--- api/index.js
+++ api/index.js
@@ -1,18 +1,32 @@
+
+import dotenv from 'dotenv'
 import express from "express";
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
 
-// mongoose.connect(process.env.MONGO).then(() => {
-//   console.log('connected to mongoDB!');
-// }).catch(err)
-
-mongoose.connect =("mongodb+srv://viveknayak8258:Vector%401437@estate-vivek.3khoqkm.mongodb.net/?retryWrites=true&w=majority")
-
+dotenv.config();
+// Initialize express app
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!!');
-    }
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-)
+// Set up a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Express server!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
